62673ddd3f21ad8ad5284f7730a81ecba06bbf53
Ben Razor <chrisbenwar1@googlemail.com>
2021-02-24 20:11:40 +0000

    Change package name. Prepare for release

---
+++ app/src/main/java/com/benrazor/peacemachine/Audio.java
@@ -0,0 +1,221 @@
+package com.benrazor.peacemachine;
+
+import android.util.Log;
+
+import com.jsyn.JSyn;
+import com.jsyn.Synthesizer;
+import com.jsyn.data.FloatSample;
+import com.jsyn.devices.android.AndroidAudioForJSyn;
+import com.jsyn.ports.UnitInputPort;
+import com.jsyn.unitgen.LineOut;
+import com.jsyn.unitgen.LinearRamp;
+import com.jsyn.unitgen.PinkNoise;
+import com.jsyn.unitgen.FilterLowPass;
+import com.jsyn.unitgen.FilterHighPass;
+import com.jsyn.unitgen.VariableRateDataReader;
+import com.jsyn.unitgen.VariableRateMonoReader;
+import com.jsyn.unitgen.VariableRateStereoReader;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Audio {
+
+    private final Synthesizer mSynth;
+    private final LinearRamp mAmpJack;
+    private final LinearRamp mLPJack;
+    // private final PinkNoise mPinkNoise;
+    private final LineOut mLineOut; // stereo output
+    private final FilterLowPass mLowPass;
+    private final FilterHighPass mHighPass;
+    private HashMap<String, FloatSample> samples = new HashMap<>();
+    private HashMap<String, PeaceMachineSource> sources = new HashMap<>();
+    AndroidAudioForJSyn androidAudioForJSyn = null;
+    private List<VibeInfo> vibeInfos;
+    private String TAG = "Audio.java";
+
+    public Audio() {
+        // Create a JSyn synthesizer that uses the Android output.
+        androidAudioForJSyn =  new AndroidAudioForJSyn();
+        mSynth = JSyn.createSynthesizer(androidAudioForJSyn);
+
+        // Create the unit generators and add them to the synthesizer.
+        mSynth.add(mAmpJack = new LinearRamp());
+        mSynth.add(mLPJack = new LinearRamp());
+        // mPinkNoise = new PinkNoise();
+        mLowPass = new FilterLowPass();
+        mHighPass = new FilterHighPass();
+
+        mSynth.add(mLPJack);
+        mSynth.add(mLowPass);
+        mSynth.add(mHighPass);
+        mSynth.add(mLineOut = new LineOut());
+
+        mLPJack.output.connect(mLowPass.frequency);
+        mLowPass.frequency.set(120.0);
+        mLowPass.Q.set(1.5);
+        mHighPass.frequency.set(140);
+        mHighPass.Q.set(1);
+
+        mAmpJack.time.set(1);
+        mAmpJack.output.connect(mHighPass.amplitude);
+
+        mLowPass.output.connect(mHighPass.input);
+        mHighPass.output.connect(0, mLineOut.input, 0);
+        mHighPass.output.connect(0, mLineOut.input, 1);
+    }
+
+    public void setLPFreqExact(float val) {
+        mLowPass.frequency.set(val);
+    }
+    public void setLPFreq(float val, float t) {
+        float freq = 80 + val * 400;
+        Log.i("Low pass freqency", Float.toString(freq));
+        mLPJack.time.set(t);
+        mLPJack.getInput().set(freq);
+        //mLowPass.frequency.set(freq);
+    }
+
+    public void setHPFreq(float val) {
+        float freq = 5 + val * 200;
+        Log.i("Low pass freqency", Float.toString(freq));
+        mHighPass.frequency.set(freq);
+    }
+
+    public void setVolume(float val, float t) {
+        mAmpJack.time.set(t);
+        mAmpJack.getInput().set(val);
+    }
+
+    public void start() {
+        mSynth.start();
+        mLineOut.start();
+    }
+
+    interface PeaceMachineSource {
+        void handleFloat(String id, float value);
+        void setLerpTimeForVolume(float time);
+        void setVolume(float volume);
+        void stop();
+    }
+
+    public class SampleSource implements PeaceMachineSource {
+        VariableRateDataReader samplePlayer;
+        private final LinearRamp ampJack;
+
+        public SampleSource(String sampleID) {
+            FloatSample sample = samples.get(sampleID);
+            int channels = sample.getChannelsPerFrame();
+            VariableRateDataReader samplePlayer = new VariableRateMonoReader();
+            if(channels == 2) {
+                samplePlayer = new VariableRateStereoReader();
+            }
+            samplePlayer.dataQueue.queueLoop(sample, 0, sample.getNumFrames());
+            mSynth.add(samplePlayer);
+            mSynth.add(ampJack = new LinearRamp());
+            ampJack.output.connect(samplePlayer.amplitude);
+
+            samplePlayer.output.connect(mLowPass.input);
+            samplePlayer.rate.set(sample.getFrameRate());
+            samplePlayer.amplitude.set(1);
+            setLerpTimeForVolume(0);
+            setVolume(0);
+            setLerpTimeForVolume(1);
+            samplePlayer.start();
+        }
+
+        public void setLerpTimeForVolume(float time) {
+            ampJack.time.set(time);
+        }
+
+        public void setVolume(float volume) {
+            ampJack.getInput().set(volume);
+        }
+
+        @Override
+        public void handleFloat(String id, float value) { }
+
+        public void stop() {
+            samplePlayer.stop();
+        }
+    }
+
+    public class NoiseSource implements PeaceMachineSource {
+        PinkNoise pinkNoise = new PinkNoise();
+        private final LinearRamp ampJack;
+
+        public NoiseSource() {
+            mSynth.add(pinkNoise);
+            mSynth.add(ampJack = new LinearRamp());
+            ampJack.output.connect(pinkNoise.amplitude);
+            setVolume(0);
+            setLerpTimeForVolume(1);
+            pinkNoise.output.connect(mLowPass.input);
+        }
+
+        public void setLerpTimeForVolume(float time) {
+            ampJack.time.set(time);
+        }
+
+        public void setVolume(float volume) {
+            ampJack.getInput().set(volume);
+        }
+
+        @Override
+        public void handleFloat(String id, float value) { }
+
+        public void stop() {
+            mSynth.stop();
+        }
+    }
+
+    public void changeVibe(String vibeID) {
+        for(Map.Entry<String, PeaceMachineSource> entry: sources.entrySet()) {
+            PeaceMachineSource source = entry.getValue();
+            source.setLerpTimeForVolume(1);
+
+            if(entry.getKey().equals(vibeID)) {
+                source.setVolume(1);
+            }
+            else {
+                source.setVolume(0);
+            }
+        }
+    }
+
+    public void addSample(String id, FloatSample sample) {
+        if(samples.get(id) == null) {
+            samples.put(id, sample);
+        }
+    }
+
+    public void addVibeInfos(List<VibeInfo> vibeInfos) {
+        this.vibeInfos = vibeInfos;
+    }
+
+    public void addVibe(VibeInfo vibeInfo) {
+        if(sources.get(vibeInfo.id) == null) {
+            PeaceMachineSource source = null;
+
+            if(vibeInfo.audio.contains(".")) {
+                source = new SampleSource(vibeInfo.id);
+            }
+            else {
+                source = new NoiseSource();
+            }
+
+            sources.put(vibeInfo.id, source);
+        }
+    }
+
+    public void destroy() {
+        Log.d(TAG, "destroy()");
+        mSynth.stop();
+        Log.d(TAG, "destroy() after mSynth.stop()");
+    }
+
+    public UnitInputPort getAmplitudePort() {
+        return mAmpJack.getInput();
+    }
+}
---
+++ app/src/main/java/com/benrazor/peacemachine/MainActivity.java
@@ -0,0 +1,219 @@
+package com.benrazor.peacemachine;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.jsyn.data.FloatSample;
+import com.jsyn.util.SampleLoader;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+
+import android.os.IBinder;
+import android.util.Log;
+
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private String TAG = "Peace Machine";
+
+    private AudioService mAudioService;
+    private Audio mAudio;
+    private boolean mServiceIsBound;
+    private WebView webView;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mAudioService = ((AudioService.LocalBinder)service).getService();
+            mAudio = mAudioService.getAudio();
+
+            onServiceCreated();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            mAudioService = null;
+            mAudio = null;
+        }
+    };
+
+    void doBindService() {
+        Intent intent = new Intent(MainActivity.this, AudioService.class);
+        startService(intent);
+
+        if (bindService(intent, mConnection, Context.BIND_AUTO_CREATE)) {
+            mServiceIsBound = true;
+        } else {
+            mServiceIsBound = false;
+            Log.e(TAG, "Error: The requested service doesn't " +
+                    "exist, or this client isn't allowed access to it.");
+        }
+    }
+
+    void doUnbindService() {
+        if (mServiceIsBound) {
+            unbindService(mConnection);
+            mServiceIsBound = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy");
+        if(mAudioService != null) {
+            if(isFinishing()) {
+                mAudio.setVolume(0, 0.05f);
+                stopService(new Intent(getApplicationContext(), AudioService.class));
+            }
+            doUnbindService();
+            mAudioService = null;
+            mAudio = null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        doBindService();
+    }
+
+    public void onServiceCreated() {
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebContentsDebuggingEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new PeaceMachineInterface(this), "PeaceMachineInterface");
+        WebSettings settings = webView.getSettings();
+        settings.setDomStorageEnabled(true);
+        if(mAudioService.audioInitialized) {
+            webView.loadUrl("file:///android_asset/web/index.html?resume=1");
+        }
+        else {
+            webView.loadUrl("file:///android_asset/web/index.html");
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * WebView.evaluateJavascript returns an escaped string wrapped in double quotes.
+     *
+     * This function turns this into a string that will be valid when passed to a JSON
+     * parser.
+     *
+     * @param s The input string
+     * @return The cleaned String
+     */
+    public String cleanReceivedJSON(String s) {
+        return s.substring(1, s.length() - 1)
+                .replace("\\\\", "\\")
+                .replace("\\\"", "\"");
+    }
+
+    public void runJS(final String js, final ValueCallback<String> valueCallback) {
+        webView.post(new Runnable() {
+            @Override
+            public void run() { webView.evaluateJavascript(js, valueCallback); }
+        });
+    }
+
+    /**
+     * Javascript interface for use by the WebView
+     */
+    public class PeaceMachineInterface {
+        Context mContext;
+        Gson gson = new Gson();
+
+        /** Instantiate the interface and set the context */
+        PeaceMachineInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void initAudio() {
+            if(!mAudioService.audioInitialized) {
+                runJS("pMachine.getVibesConfig()" , new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String s) {
+                        s = cleanReceivedJSON(s);
+                        List<VibeInfo> vibeInfos = gson.fromJson(s, new TypeToken<List<VibeInfo>>(){}.getType());
+                        mAudio.addVibeInfos(vibeInfos);
+
+                        for (VibeInfo vibeInfo : vibeInfos) {
+                            if(vibeInfo.audio.contains(".")) {
+                                loadSample(vibeInfo.id, vibeInfo.audio);
+                            }
+                            mAudio.addVibe(vibeInfo);
+                        }
+
+                    }
+                });
+                mAudioService.audioInitialized = true;
+            }
+        }
+
+        @JavascriptInterface
+        public void handleFloat(String control, float val, float t) {
+            if(control.equals("pm-control-downers")) {
+                mAudio.setLPFreq(val, t);
+            }
+            else if(control.equals("pm-control-uppers")) {
+                mAudio.setVolume(val, t);
+            }
+        }
+
+        @JavascriptInterface
+        public void selectVibe(final String vibeID) {
+            Log.d(TAG, "selectVibe()");
+            mAudio.changeVibe(vibeID);
+        }
+
+        public void loadSample(String id, String path) {
+            Log.d(TAG, "loadSample()");
+            try {
+                InputStream iStr = getAssets().open("web/audio/" + path);
+                FloatSample sample = SampleLoader.loadFloatSample(iStr);
+                mAudio.addSample(id, sample);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @JavascriptInterface
+        public void turnOn() {
+            runJS("pMachine.handleTurnOn()", null);
+        }
+
+        @JavascriptInterface
+        public void turnOff() {
+            Log.d(TAG, "turnOff");
+            finishAffinity();
+        }
+    }
+}
